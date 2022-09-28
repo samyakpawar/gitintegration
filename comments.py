92f0b46a4a623d0f938c2d8ce3280bc89ba968a6
samyakpawar <114568740+samyakpawar@users.noreply.github.com>
2022-09-28 15:41:40 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,54 @@
+## Shortcuts
+## ctrl + k + c => to comment multiple lines
+## ctrl + k + u => to uncomment multiple lines
+
+## ctr + /  => toggle , comment or uncomment 
+
+###   /  => forward slash ## look for 
+###   \  => backward slash
+
+
+
+# First sample program
+# Developer : Credence CT11
+# Created date : 12-Aug-2022
+
+print("Hello Credence")     # to print some text on console
+
+#### --------------------------------------
+
+# - single line comment
+# '''Comment'''  - # multiline comment
+
+# """Comments"""  - # multiline comment
+
+
+'''First sample program
+Developer : Credence CT11
+Created date : 12-Aug-2022'''
+
+
+"""First sample program
+Developer : Credence CT11
+Created date : 12-Aug-2022"""
+
+print("Hello Credence")
+
+
+# First sample program
+# Developer : Credence CT11
+# Created date : 12-Aug-2022
+
+
+print(10 * 1)
+print(10 * 2)
+# print(10 * 3)
+# print(10 * 4)
+print(10 * 5)
+print(10 * 6)
+print(10 * 7)
+print(10 * 8)
+print(10 * 9)
+print(10 * 10)
+
+
